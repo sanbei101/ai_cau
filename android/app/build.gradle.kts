d60f29f669514d2b8aec8cdf52b5b83d624190b7
sanbei101 <sanbei101@outlook.com>
2025-11-23 00:30:44 +0800

    1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,9 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
     }
 
     buildTypes {
@@ -35,6 +38,7 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            shrinkResources true
         }
     }
 }
