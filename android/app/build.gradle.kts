33cb6a4e612a2db90a63a993a85f91ebed939d31
sanbei101 <sanbei101@outlook.com>
2025-11-23 14:08:08 +0800

    1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,17 +20,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "ai.sanbei101.xyz.ai_cau"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        ndk {
-            abiFilters += listOf("arm64-v8a")
-        }
     }
 
     buildTypes {
